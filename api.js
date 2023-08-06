96a13b9529ac759e39d8fe736dddd8fdac0ccfd8
Remku <ngancloverr+remkugithub@gmail.com>
2023-08-06 19:05:56 +0530

    some comments added

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import endpoints from './endpoints'
 
 const Axios = axios.create({
+  // TODO: change the baseUrl when in production
   baseURL: 'http://192.168.0.103:3000/api',
   headers: {
     'Content-Type': 'application/json',
